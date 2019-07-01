54de7cd7332bf53af2e88d1a02b465620f11e721
Rimantas <belauzas@yahoo.com>
2019-07-01 10:17:41 +0300

    Kintamuju iniciavimas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
-console.log('hello');
+"use strict";
+
+var a = 1;
+console.log( a );
+
+var b = 2;
+console.log( b );
+
+var c = 4;
+console.log( c );
+
+var txt_a = 'tekstas 1';
+console.log( txt_a );
+
+var txt_b = "tekstas 2";
+console.log( txt_b );
+
+var txt_c = 'tekstas 3';
+console.log( txt_c );
+
+var marks = [10, 2, 7, 7, 8];
+console.log( marks );
+
+var worst_marks = [1, 2, 2, 1, 2];
+console.log( worst_marks );
+
+var usernames = ['asd', 'qwe', "''", '""', 'kitkat'];
+console.log( usernames );
+
+var sakiniai = [
+    'Pamidoras yra raudonas.',
+    'Cia yra panaudotos "dvygubos" kabutes.',
+    "Cia yra panaudotos 'viengubos' kabutes.",
+    "Cia yra panaudotos \"dvygubos\" kabutes.",
+    'Cia yra panaudotos \'viengubos\' kabutes.'
+];
+console.log(sakiniai);
+
+var tiesa = true;
+console.log( tiesa );
+
+var melas = false;
+console.log( melas );
+
