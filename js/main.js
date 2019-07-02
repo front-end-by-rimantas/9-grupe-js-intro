1055e8078e8f678c24b0471315e4023c12494eeb
Rimantas <belauzas@yahoo.com>
2019-07-02 10:53:39 +0300

    Remove console

--- js/main.js
+++ js/main.js
@@ -174,14 +174,14 @@ console.log('Atbulas tekstas: ' + atbulas);
 console.log('-------------');
 console.log('Dalyba be liekanos intervale');
 
-var nuo = 0,
-    iki = 11,
-    daliklis = 4,
+var nuo = -180,
+    iki = 180,
+    daliklis = 5,
     kiekis = 0;
 
 // logika
 for ( var i=nuo; i<=iki; i++ ) {
-    console.log( i, '%', daliklis, ' = ', (i - i%daliklis) / daliklis, 'sveiku ir ', i%daliklis, 'liekana' );
+    // console.log( i, '%', daliklis, ' = ', (i - i%daliklis) / daliklis, 'sveiku ir ', i%daliklis, 'liekana' );
     
     if ( i % daliklis === 0 ) {
         kiekis++;
